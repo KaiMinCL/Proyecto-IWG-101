7784034cd39b6b578ddd2f44a6623eac17554cb8
David Álvarez <36682968+KaiMinCL@users.noreply.github.com>
2019-12-29 15:16:05 -0300

    Implementación funciones
    Se implementó el ScrollView y el ScreenManager

--- Main/main.py
+++ Main/main.py
@@ -1,11 +1,13 @@
 import kivy
 from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
-from kivy.core.window import Window
 from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.button import ButtonBehavior
+from kivy.uix.button import Button, ButtonBehavior
+from kivy.uix.screenmanager import ScreenManager, Screen
+
 
 Window.size = 360, 640
 Window.clearcolor = 1,1,1,1
@@ -13,15 +15,24 @@ Window.clearcolor = 1,1,1,1
 class GridButton(ButtonBehavior, GridLayout):
     pass
 
-class MainGrid(GridLayout):
+class MainWindow(Screen):
     def mycall_back(self):
-        print("Este boton esta funcionando")
+        print("This button is working")
     pass
+
+class RecipesWindow(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv=Builder.load_file("main.kv")
 
 
 class MainApp(App):
+    title= "Como en Casa"
     def build(self):
-        return MainGrid()
+        return kv
 
 if __name__ == "__main__":
-    MainApp().run()
+    MainApp().run() 
